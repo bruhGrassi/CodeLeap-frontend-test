6d3fff6d8d18a8fb293fa0bf0b38359290e8a7e8
Bruna Grassi <brunagm88@gmail.com>
2026-02-11 12:12:26 -0300

    chore: add PostCard component

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,4 +1,5 @@
 import PostForm from "./PostForm";
+import PostCard from "./PostCard";
 
 function MainScreen() {
   return(
@@ -10,8 +11,32 @@ function MainScreen() {
           CodeLeap Network
         </h1>
 
-        <section className="w-full bg--neutral-50 p-4">
+        <section className="w-full bg--neutral-50 p-4 grid gap-4">
           <PostForm />
+
+          <PostCard
+            title="My first post at Codeleap NetWork!"
+            username="Victor"
+            timestamp="25 minutes ago"
+            content={`Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                      
+                      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                      
+                      It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`}
+            isOwner={true}
+          />
+
+          <PostCard
+            title="My first post at Codeleap NetWork!"
+            username="Vini"
+            timestamp="45 minutes ago"
+            content={`Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                      
+                      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                      
+                      It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`}
+            isOwner={false}
+          />
         </section>
       </main>
     </div>
