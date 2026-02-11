c9f7e2ea8f3991ca39ed33dc39e1b015cad7748a
Bruna Grassi <brunagm88@gmail.com>
2026-02-11 11:08:46 -0300

    chore: create PostForm component

---
+++ src/components/PostForm.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import FormHeader from './ui/FormHeader'
+import FormInput from './ui/FormInput'
+import FormTextarea from './ui/FormTextarea'
+import FormButton from './ui/FormButton'
+
+function PostForm() {
+  const [form, setForm] = useState({
+    title: '',
+    content: '',
+  })
+
+  const isDisabled = Object.values(form).some(value => value.trim() === '');
+
+  return (
+    <form className="bg-neutral-50 rounded-2xl p-6 w-full border border-neutral-300">
+      <FormHeader title="What’s on your mind?" />
+      
+      <FormInput
+        label="Title"
+        placeholder="Hello world"
+        value={form.title}
+        onChange={(e) =>
+          setForm(prev => ({
+            ...prev,
+            title: e.target.value,
+          }))
+        }
+      />
+
+      <div className="pt-4">
+        <FormTextarea
+          label="Content"
+          placeholder="Content here"
+          value={form.content}
+          onChange={(e) =>
+            setForm(prev => ({
+              ...prev,
+              content: e.target.value,
+            }))
+          }
+        />
+      </div>
+
+      <div className="flex justify-end mt-4">
+        <FormButton disabled={isDisabled}>
+          Create
+        </FormButton>
+      </div>
+    </form>
+  )
+}
+
+export default PostForm;
